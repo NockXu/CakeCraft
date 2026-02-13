ff248b6c75762e746908724d1b31a8bc47f0e704
NockXu <gabriel.dav@outlook.fr>
2026-02-13 16:12:10 +0100

    feat(screen): ajout de l'écran

---
+++ src/screen.py
@@ -0,0 +1,26 @@
+import pygame
+
+class Screen:
+    def __init__(self):
+        self.width = 1280
+        self.height = 1024
+        self.screen = pygame.display.set_mode((self.width, self.height), pygame.FULLSCREEN | pygame.SCALED)
+        pygame.display.set_caption("CakeCraft")
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = Screen()
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    running = False
+        
+        screen.screen.fill((255, 255, 255))
+        pygame.display.flip()
+    
+    pygame.quit()
